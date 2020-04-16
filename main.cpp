20d5b7c53410381eab8878081b07df3a16d3281e
ShinbumKang <21500005@handong.edu>
2020-04-16 03:20:24 -0700

    hw2-1 done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,20 @@ int sorted[10];
 
 int main(){
 
+    
+    int data[10],i;
+    for(i=0;i<10;i++)
+    {
+        cin>>data[i];
+    }
+
+    merge_sort(data, 0, 9);
+
+    for(i=0;i<10;i++)
+    {
+        cout<<data[i]<<" ";
+    
+    }
+    cout<<endl;
     return 0;
 }
